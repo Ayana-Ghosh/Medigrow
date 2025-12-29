f2d289057a50b5fc2be49adc635a7c8de88029fa
Ayana Ghosh <159642466+Ayana-Ghosh@users.noreply.github.com>
2025-12-29 18:16:22 +0530

    Update newfooter.php

--- newfooter.php
+++ newfooter.php
@@ -1,7 +1,7 @@
 <div id="footer-sec">
 
     <a href="aboutus.php">
-        <h5>| Design and Developed By : Student's of TIT Tripura |&nbsp;&nbsp; Anushka Banik.(27/CS/22/01),&nbsp; Ayana Ghosh.(27/CS/22/02),&nbsp; Tammanna Debnath.(27/CS/22/03), &nbsp;Shreshtha Pal.(27/CS/22/09),&nbsp; Ankan Deb.(27/CS/22/16),&nbsp; Bishal Debnath.(27/CS/22/30)</h5>
+        <h5>| Design and Developed By : &nbsp; Ayana Ghosh |&nbsp; As Diploma-Final Year project 2021-22 |</h5>
     </a>
 </div>
 
